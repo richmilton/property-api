6ca32cc5b1d00891e748d98bc7561184d1bab5b1
richard <richardmilton@ahotclub.com>
2019-04-04 22:50:31 +0100

    add email access white list

--- routes/comparisons.js
+++ routes/comparisons.js
@@ -7,14 +7,23 @@ const {
   deleteComparison,
 } = require('../data');
 
-const setHeaders = (res, req) => {
+const setHeaders = (res, req, isOptions) => {
+  const { email = ''} = req.params.email ? req.params : req.body;
   const whiteList = process.env.ORIGIN_WHITE_LIST.split('~');
+  const emailWhiteList = process.env.EMAIL_WHITE_LIST.split('~');
+  if (!isOptions && emailWhiteList.indexOf(email) === -1) {
+    res.status(403).end();
+    return false;
+  }
   const origin = req.get('origin');
   if (whiteList.indexOf(origin) !== -1) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    return true;
   }
+  res.status(403).end();
+  return false;
 };
 
 const errorMap = {
@@ -27,48 +36,53 @@ const getError = (errCode) => {
 };
 
 router.get('/:id', (req, res) => {
-  setHeaders(res, req);
-  console.log(req.params.id);
-  getComparison(req.params.id, res);
+  if (setHeaders(res, req)) {
+    console.log(req.params.id);
+    getComparison(req.params.id, res);
+  }
 });
 
-router.get('/', (req, res) => {
-  setHeaders(res, req);
-  getComparisons(
-    (errCode) => {
-      const { status, message } = getError(errCode);
-      res.status(status).send(message);
-    },
-    (data) => res.send(data)
-  );
+router.get('/user/:email', (req, res) => {
+  if (setHeaders(res, req)) {
+    getComparisons(
+      (errCode) => {
+        const {status, message} = getError(errCode);
+        res.status(status).send(message);
+      },
+      (data) => res.send(data)
+    );
+  }
 });
 
 router.put('/', (req, res) => {
-  setHeaders(res, req);
-  createComparison(req.body,
-    (errCode) => {
-      const { status, message } = getError(errCode);
-      res.status(status).send(message);
-    },
-    (data) => res.send(data)
-  );
+  if (setHeaders(res, req)) {
+    createComparison(req.body,
+      (errCode) => {
+        const {status, message} = getError(errCode);
+        res.status(status).send(message);
+      },
+      (data) => res.send(data)
+    );
+  }
 });
 
 router.delete('/', (req, res) => {
-  setHeaders(res, req);
-  deleteComparison(
-    req.body.id,
-    (errCode) => {
-      const { status, message } = getError(errCode);
-      res.status(status).send(message);
-    },
-    (respCode) => res.send(respCode)
-  );
+  if (setHeaders(res, req)) {
+    deleteComparison(
+      req.body.id,
+      (errCode) => {
+        const {status, message} = getError(errCode);
+        res.status(status).send(message);
+      },
+      (respCode) => res.send(respCode)
+    );
+  }
 });
 
 router.options('/', (req, res) => {
-  setHeaders(res, req);
-  res.send(200);
+  if (setHeaders(res, req, true)) {
+    res.send(200);
+  }
 });
 
 module.exports = router;
